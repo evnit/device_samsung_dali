1c10449155cc18d82ad6ac5c447e15a4b9f70e76
Le Anh Dung <evnithn@gmail.com>
2015-07-27 08:43:49 +0700

    SamsungDozeService: Prevent a pm wakeup with a timestamp in the future

--- qcom-common/doze/src/com/cyanogenmod/settings/device/SamsungDozeService.java
+++ qcom-common/doze/src/com/cyanogenmod/settings/device/SamsungDozeService.java
@@ -32,7 +32,9 @@ import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
+import java.lang.System;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.List;
 
 public class SamsungDozeService extends Service {
@@ -43,6 +45,7 @@ public class SamsungDozeService extends Service {
 
     private static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     private static final String GESTURE_POCKET_KEY = "gesture_pocket";
+    private static final String PROXIMITY_WAKE_KEY = "proximity_wake_enable";
 
     private static final int POCKET_DELTA_NS = 1000 * 1000 * 1000;
 
@@ -52,6 +55,7 @@ public class SamsungDozeService extends Service {
 
     private boolean mHandwaveGestureEnabled = false;
     private boolean mPocketGestureEnabled = false;
+    private boolean mProximityWakeEnabled = false;
 
     class SamsungProximitySensor implements SensorEventListener {
         private SensorManager mSensorManager;
@@ -84,10 +88,18 @@ public class SamsungDozeService extends Service {
         }
 
         private boolean shouldPulse(long timestamp) {
+            // Never pulse if the timestamp is from the future
+            if (timestamp > System.nanoTime()) {
+                return false;
+            }
+
             long delta = timestamp - mInPocketTime;
 
             if (mHandwaveGestureEnabled && mPocketGestureEnabled) {
                 return true;
+            } else if (mProximityWakeEnabled && (delta < POCKET_DELTA_NS)) {
+                mPowerManager.wakeUp(TimeUnit.NANOSECONDS.toMillis(timestamp));
+                return false;
             } else if (mHandwaveGestureEnabled && !mPocketGestureEnabled) {
                 return delta < POCKET_DELTA_NS;
             } else if (!mHandwaveGestureEnabled && mPocketGestureEnabled) {
@@ -96,8 +108,9 @@ public class SamsungDozeService extends Service {
             return false;
         }
 
-        public void enable() {
-            if (mHandwaveGestureEnabled || mPocketGestureEnabled) {
+        public void testAndEnable() {
+            if ((isDozeEnabled() && (mHandwaveGestureEnabled || mPocketGestureEnabled)) ||
+                    mProximityWakeEnabled) {
                 mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
             }
         }
@@ -116,8 +129,8 @@ public class SamsungDozeService extends Service {
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
         loadPreferences(sharedPrefs);
         sharedPrefs.registerOnSharedPreferenceChangeListener(mPrefListener);
-        if (!isInteractive() && isDozeEnabled()) {
-            mSensor.enable();
+        if (!isInteractive()) {
+            mSensor.testAndEnable();
         }
     }
 
@@ -155,14 +168,13 @@ public class SamsungDozeService extends Service {
 
     private void onDisplayOff() {
         if (DEBUG) Log.d(TAG, "Display off");
-        if (isDozeEnabled()) {
-            mSensor.enable();
-        }
+        mSensor.testAndEnable();
     }
 
     private void loadPreferences(SharedPreferences sharedPreferences) {
         mHandwaveGestureEnabled = sharedPreferences.getBoolean(GESTURE_HAND_WAVE_KEY, false);
         mPocketGestureEnabled = sharedPreferences.getBoolean(GESTURE_POCKET_KEY, false);
+        mProximityWakeEnabled = sharedPreferences.getBoolean(PROXIMITY_WAKE_KEY, false);
     }
 
     private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
@@ -184,6 +196,8 @@ public class SamsungDozeService extends Service {
                 mHandwaveGestureEnabled = sharedPreferences.getBoolean(GESTURE_HAND_WAVE_KEY, false);
             } else if (GESTURE_POCKET_KEY.equals(key)) {
                 mPocketGestureEnabled = sharedPreferences.getBoolean(GESTURE_POCKET_KEY, false);
+            } else if (PROXIMITY_WAKE_KEY.equals(key)) {
+                mProximityWakeEnabled = sharedPreferences.getBoolean(PROXIMITY_WAKE_KEY, false);
             }
         }
     };
